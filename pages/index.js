5ce8f61c00b2c5e6cc8327de0224136e204ddd5b
Robert <robert.mabry86@gmail.com>
2023-04-25 21:41:51 -0500

    updated index file

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
+import { signOut } from '../utils/auth';
+import { getTeams } from '../api/teamData';
+import TeamCard from '../components/TeamCard';
 
 function Home() {
+  const [teams, setTeams] = useState([]);
   const { user } = useAuth();
+  const getAllTeams = () => {
+    getTeams(user.uid).then(setTeams);
+  };
+  useEffect(() => {
+    getAllTeams();
+  }, []);
 
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
+    <div className="text-center my-4">
+      <Link href="/teams/new" passHref>
+        <Button>Add A TEAM</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {/* TODO: map over teams using TeamsCard component */}
+        {teams.map((team) => (
+          <TeamCard key={team.firebaseKey} teamObj={team} onUpdate={getAllTeams} />
+        ))}
+        <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
+          Sign Out
+        </Button>
+      </div>
     </div>
   );
 }
